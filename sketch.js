6685021786d8359549300bd7ae25056c57828035
Tom Sealey <tpsealey@gmail.com>
2018-09-06 11:56:34 +0100

    tweked some settings

--- sketch.js
+++ sketch.js
@@ -7,13 +7,15 @@
 // https://youtu.be/67k3I2GxTH8
 
 let values = [];
-
 let loops = 0;
+let swaps = 0;
+let lines = false;
+let numLines = 50;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	colorMode(HSB, width);
-	for (i = 0; i < width; i++) {
+	colorMode(HSB, height);
+	for (i = 0; i < numLines; i++) {
 		values[i] = random(height);
 		//values[i] = noise(i/100.0)*height;
 	}
@@ -23,22 +25,42 @@ function draw() {
 	background(0);
 
 	if (loops < values.length) {
-		for (j = 0; j < values.length - loops - 1; j++) {
-			let a = values[j];
-			let b = values[j + 1];
+		// for (j = 0; j < values.length - loops - 1; j++) {
+		// 	let a = values[j];
+		// 	let b = values[j + 1];
+		// 	if (a > b) {
+		// 		swap(values, j, j + 1);
+		// 	}
+		// }
+		if (swaps < values.length - loops - 1) {
+			let a = values[swaps];
+			let b = values[swaps + 1];
 			if (a > b) {
-				swap(values, j, j + 1);
+				swap(values, swaps, swaps + 1);
 			}
+			swaps++;
+		} else {
+			console.log('swap complete, restarting');
+			swaps = 0;
+			loops++;
 		}
+
 	} else {
 		console.log("finished");
 		noLoop();
 	}
-	loops++;
 
 	for (i = 0; i < values.length; i++) {
-		stroke(i,width, width);
-		line(i, height, i, height - values[i]);
+		let col = color(values[i], height, height);
+		let location = map(i, 0, values.length, 0, width);
+		stroke(col);
+		if (lines) {
+			line(location, height, location, height - values[i]);
+		} else {
+			noStroke();
+			fill(col);
+			rect(location, height - values[i], width / numLines-1, height);
+		}
 	}
 }
 
